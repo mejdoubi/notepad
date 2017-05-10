f548745d3c8476868177935654e15973800b9937
mejdoubi <o.mejdoubi@gmail.com>
2017-05-10 19:39:25 +0200

    final version

--- src/NoteBundle/Controller/DefaultController.php
+++ src/NoteBundle/Controller/DefaultController.php
@@ -2,18 +2,9 @@
 
 namespace NoteBundle\Controller;
 
-use NoteBundle\Entity\Note;
-use NoteBundle\Entity\Category;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
-use Symfony\Component\Form\Extension\Core\Type\TextareaType;
-use Symfony\Component\Form\Extension\Core\Type\ButtonType;
-use Symfony\Component\Form\Extension\Core\Type\DateType;
-use Symfony\Component\Form\Extension\Core\Type\SubmitType;
-use Symfony\Component\Form\Extension\Core\Type\SearchType;
-use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 
 class DefaultController extends Controller
 {
@@ -22,7 +13,12 @@ class DefaultController extends Controller
      */
     public function showHomeAction()
     {
+        /* ANGULAR */
+        return $this->render('NoteBundle:layout_angular.html.twig');
+        
+        /* SYMFONY
         return $this->redirectToRoute('notes');
+        */
     }
 }
 
